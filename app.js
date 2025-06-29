af70d32d6b39d44ea37401414342cc39e6e825b4
Carlos Vinicius <carlosviniciuslazarosilva777@gmail.com>
2025-06-29 02:53:27 -0300

    versão final

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const app = express();
-const routes = require('./routes'); // o arquivo que contém seu código acima
+const cors = require('cors');
+const dotenv = require('dotenv');
+const routes = require('./routes');
 
-app.use(express.json()); // middleware global para JSON
-app.use('/api', routes);
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api', routes); // prefixa as rotas com /api
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
--- db.js
+++ db.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
 const mysql = require('mysql2/promise');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
@@ -7,9 +9,6 @@ const pool = mysql.createPool({
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQL_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 module.exports = pool;
